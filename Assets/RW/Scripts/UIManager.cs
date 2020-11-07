a17099a82db774376747f98117fd13e05c37f3df
Dkrv <70618651+kiuuu00@users.noreply.github.com>
2020-11-06 18:07:15 -0600

    Calling StartGame When the Button is Clicked
    can't choose UIManager in On click ()

---
+++ Assets/RW/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public void StartGame()
+{
+    SceneManager.LoadScene("RocketMouse");
+}
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void
+    {
+        
+    }
+
+public void StartGame()
+{
+    SceneManager.LoadScene("RocketMouse");
+}
+
+// Update is called once per frame
+void
+{
+        
+    }
+}
