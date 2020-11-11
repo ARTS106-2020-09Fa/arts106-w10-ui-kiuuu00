a292a1b0acc6065e2ad6f649549b226b3d56d5d2
Dkrv <70618651+kiuuu00@users.noreply.github.com>
2020-11-11 14:04:57 -0600

    mouse spritemap
    ...

---
+++ Assets/UILayout/InventoryDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class InventoryDisplay : MonoBehaviour
+{
+    public void ShowOnly(int itemType)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            InventoryItemButton thisItem = transform.GetChild(i).GetComponent<InventoryItemButton>();
+            thisItem.gameObject.SetActive(thisItem.typeIndex == itemType);
+        }
+    }
+
+
+    public void ShowAll()
+    {
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/UILayout/InventoryItemButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemButton : MonoBehaviour
+{
+    private Text buttonText;
+    private string[] itemTypes = { "Armor", "Weapon", "Spell" };
+    public int typeIndex;
+
+    void Awake()
+    {
+        typeIndex = UnityEngine.Random.Range(0, 3);
+        buttonText = GetComponentInChildren<Text>();
+        buttonText.text = itemTypes[typeIndex];
+    }
+
+    
+}
